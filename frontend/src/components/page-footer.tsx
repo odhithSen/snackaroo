ef991f8ee626ae645b5dd9bea5a561708517e899
OdhithSen <odhith.sen@gmail.com>
2024-10-08 00:54:19 +0530

    Initialize tailwind css

--- frontend/src/components/page-footer.tsx
+++ frontend/src/components/page-footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const PageFooter = () => {
   return (
     <footer>
-      <h1>footer place holder</h1>
+      <h1 className="underline">footer place holder</h1>
     </footer>
   );
 };
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./app";
 import { Auth0ProviderWithNavigate } from "./auth0-provider-with-navigate";
+import "./index.css";
 
 const container = document.getElementById("root") as HTMLElement;
 
